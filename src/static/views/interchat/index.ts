ee17f6bb6aab8e5dfc484414654a0621ce3f3ea0
tnfAngel <sniperhalfing@gmail.com>
2021-12-17 14:05:09 +0000

    fix: 🐛 Routes not rendering. Body path fix

--- src/static/views/interchat/index.ts
+++ src/static/views/interchat/index.ts
@@ -4,8 +4,8 @@ module.exports = (ctx) => /*html*/ `
   <div class="container">
     <a href="https://chatglobal.ml/" class="brand"><img src="https://cdn.chatglobal.ml/assets/logo.png" alt="Logo" width="140" height="140"></a>
 
-    <h2>Viendo post ${decodeURIComponent(ctx.params.id)}</h2>
-    <h1>Sasilla.</h1>
+    <h2>Viendo interchat ${decodeURIComponent(ctx.params.id)}</h2>
+    <h1>Qu.</h1>
 
     <p>Que tal</p>
 
